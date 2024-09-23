cc908bb3048b7c5c696e60b7fe109ec161ad41fa
Karthikdara10 <darakarthik10@gmail.com>
2024-09-23 18:03:20 +0530

    Add files via upload

---
+++ none.py
@@ -0,0 +1,9 @@
+>>> a=None
+>>> print(a,type(a))--------------None <class 'NoneType'>
+>>> None==0----------------------False
+>>> None==""--------------------False
+>>> None==False----------------False
+>>> None==None----------------True
+>>> n=NoneType()------------------------NameError: name 'NoneType' is not defined
+>>>print({}.clear())-------------------None
+>>>print([].clear())-------------------None
