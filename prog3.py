db434e19360aac2a69e5dee28454de674aac6882
Karthikdara10 <darakarthik10@gmail.com>
2024-09-23 13:07:30 +0530

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,14 @@
+>>> x=2+3j
+>>> print(x,type(x),id(x))
+(2+3j) <class 'complex'> 2894582726256
+>>> print(x.real)
+2.0
+>>> print(x.imag)
+3.0
+>>> x=0+0j
+>>> print(x,type(x),id(x))
+0j <class 'complex'> 2894582710864
+>>> print(x.real)
+0.0
+>>> print(x.imag)
+0.0
