6aa69304e6aeaeaba9562788ab22da276a2f29c4
Karthikdara10 <darakarthik10@gmail.com>
2024-09-23 13:13:34 +0530

    Add files via upload

---
+++ prog5.py
@@ -0,0 +1,21 @@
+>>> x="python programming"
+>>> print(x,type(x),id(x))
+python programming <class 'str'> 2894582841776
+>>> x="100"
+>>> print(x,type(x),id(x))
+100 <class 'str'> 2894582837056
+>>> x="2+3j"
+>>> print(x,type(x),id(x))
+2+3j <class 'str'> 2894582832400
+>>> x="2.32"
+>>> print(x,type(x),id(x))
+2.32 <class 'str'> 2894582827456
+>>> x='''Guido van rossum
+... H-no 12-34,seaside
+... numpy organisation
+... netherland-58'''
+>>> print(x,type(x),id(x))
+Guido van rossum
+H-no 12-34,seaside
+numpy organisation
+netherland-58 <class 'str'> 2894584219888
