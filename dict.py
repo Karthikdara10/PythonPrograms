2a0b5b4e4262f04be6c411124c88e874396ed1ac
Karthikdara10 <darakarthik10@gmail.com>
2024-09-23 18:01:12 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,10 @@
+>>> d1={1:"apple",2:"banana",3:"strawberry",4:"guava"}
+>>> print(d1,type(d1))
+{1: 'apple', 2: 'banana', 3: 'strawberry', 4: 'guava'} <class 'dict'>
+>>> d1[1]
+'apple'
+>>> d1[2]
+'banana'
+>>> d1[3]
+'strawberry'
+>>>
